bb92cd0a0847290ffcec511f37a090a8b5f7bbe4
Istiak Shihab <istiak@pm.me>
2022-07-05 16:47:00 +0600

    Resume Added

--- src/components/ResumeButton.js
+++ src/components/ResumeButton.js
@@ -2,8 +2,17 @@ import React from 'react';
 import RetroButton from './RetroButton';
 import '../styles/Button-Placement.css';
 
+import resume from '../content/resume.txt';
+
+let ResumeText = "Loading";
+
 class ResumeButton extends React.Component {
     render() {
+        fetch(resume)
+        .then(r => r.text())
+        .then(text => {
+            ResumeText = text
+        })
 
         const { toggleContent } = this.props;
         const { setInnerHtml } = this.props;
@@ -24,7 +33,7 @@ class ResumeButton extends React.Component {
                             else {
                                 toggleContent(true)
                                 setActivePage(activeButton.RESUME)
-                                setInnerHtml("PublicationText")
+                                setInnerHtml(ResumeText)
                             }
                         }
                     } />
